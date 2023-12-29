a908a18c6cf868c83fc370fe9a5465e7eb833f5b
joshsack1 <80927517+joshsack1@users.noreply.github.com>
2023-12-29 12:09:13 -0500

    Created functions to analyse data

--- data.jl
+++ data.jl
@@ -1,7 +1,6 @@
-#%% Include analytical methods
-include("analysis.jl")
 #%% Read in data
-using FredData
+using FredData, DataFrames
+using ShiftedArrays: lag
 # Note that I have set an environment variable FRED_API_KEY
 #%%
 key = ENV["FRED_API_KEY"]
@@ -26,3 +25,28 @@ function add_lags!(df::DataFrame, lag_length::Int=24)
 end
 #%% Using these lagged values, add columns for the annualized percent change in the index at each lag
 # THIS SHOULD RETURN A NEW DATAFRAME
+"A function to calculate the annualized percent change based on a lag"
+function annualized_percent_change(current::Vector{Float64}, lagged::Vector{Float64}, lags::Int64)
+    percent_change = (current .- lagged) ./ lagged
+    apc = ((1 .+ percent_change) .^ (12/lags)) .- 1
+    return 100.0 .* apc
+end
+
+"A function to create a dataframe of annualized percent changes based on a dataframe of lags"
+function apc_df(df::DataFrame)
+    lags = size(df, 2) - 2
+    clean_df = dropmissing!(df[lags+1:end, :])
+    current = clean_df[:, :value]
+    lag_collection = []
+    for i in 1:lags 
+        string_name = "l" * string(i)
+        symbol_name = Symbol(string_name)
+        push!(lag_collection, clean_df[:, symbol_name])
+    end
+    inter = annualized_percent_change.(Ref(current), lag_collection, 1:lags)
+    data_mat = hcat(inter...)
+    col_names = ["pi" * string(i) * "m" for i in 1:lags]
+    rdf = DataFrame(data_mat, col_names)
+    rdf.date = clean_df.date
+    return rdf
+end
