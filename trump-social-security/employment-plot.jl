c041c9fc8447ac100ba64092c313cbe9e727cc03
joshsack1 <80927517+joshsack1@users.noreply.github.com>
2024-02-13 10:24:24 -0500

    Added new plot

---
+++ trump-social-security/employment-plot.jl
@@ -0,0 +1,26 @@
+#%% Include packages
+using Plots, FredData, DataFrames, Dates, PlotThemes
+#%% Set the plot theme as mute
+theme(:mute)
+#%% Pull data from FRED
+key = ENV["FRED_API_KEY"]
+f = Fred(key)
+#%% Pull employment data
+data =
+    get_data(f, "PAYEMS"; observation_start="2016-07-01", observation_end="2022-07-01").data
+#%% Plot the data
+emp_plot = plot(
+    data.date,
+    data.value;
+    title="Employment Declined Under Trump",
+    xlabel="Date",
+    ylabel="All Nonfarm Employees (thousands)",
+    label="Employment",
+    formatter = :plain,
+    legend = :bottomleft
+)
+vline!(emp_plot, [Date(2017, 1, 20)], label="Trump Inauguration")
+vline!(emp_plot, [Date(2021, 1, 20)], label="Biden Inauguration")
+hline!(emp_plot, [data.value[7]], label = " ")
+#%% Save the figure
+savefig(emp_plot, "trump-social-security/employment-plot.png")
